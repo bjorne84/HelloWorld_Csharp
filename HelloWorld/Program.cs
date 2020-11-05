b9cb5095c6a0eb5da66c4449988dc6fcfcec54a8
bjorne84 <edinbjorn@gmail.com>
2020-11-05 10:04:01 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+
+   
+
+    }
+}
+
+
